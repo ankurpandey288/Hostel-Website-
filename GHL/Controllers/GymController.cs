ef4af78468f2f8002085cdd3f3ee5e0614f807cb
Ankur kumar pandey <77723412+ankurpandey288@users.noreply.github.com>
2022-01-20 12:18:28 +0530

    initial cmommit

---
+++ GHL/Controllers/GymController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GHL.Controllers
+{
+    public class GymController : Controller
+    {
+        // GET: Gym
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Amenities()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ GHL/Controllers/LibraryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GHL.Controllers
+{
+    public class LibraryController : Controller
+    {
+        // GET: Library
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Amenities()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
